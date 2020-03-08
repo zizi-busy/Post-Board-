892aca52921d802621b6fa99d2076dcd8a6b8146
zizi-busy <inka.zr@gmail.com>
2020-03-08 15:07:09 +0200

    classes added

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -22,31 +22,31 @@ class AddPost extends Component {
    } 
     render (){
         return (
-            <div class = "post-container">
+            <div className = "post-container">
            
                 <form onSubmit = {this.handleSubmit}>
                     <h1>New Post</h1>
-                <div class="form-inner">
+                <div className="form-inner">
         
-                    <div class="input-holder">
-                        <span class="label"> Title:</span>
-                         <input class = "input-text" type="text" id="title" onChange={this.handleChange}/>
+                    <div className="input-holder">
+                        <span className="label"> Title:</span>
+                         <input className = "input-text" type="text" id="title" onChange={this.handleChange}/>
                          
                     
                      </div>
         
-                    <div class="input-holder">
-                        <span class="label"> Category:</span>
-                        <input class="input-text" type="text" id="category"onChange={this.handleChange}/>
+                    <div className="input-holder">
+                        <span className="label"> Category:</span>
+                        <input className="input-text" type="text" id="category"onChange={this.handleChange}/>
                      
                     </div>  
                         
-                    <div class = "input-holder">
-                        <span class="label"> Write new post:</span>  
-                        <textarea class="input-text" value={this.state.value} type="text" id="newPost" onChange={this.handleChange}></textarea> 
+                    <div className = "input-holder">
+                        <span className="label"> Write new post:</span>  
+                        <textarea className="input-text" value={this.state.value} type="text" id="newPost" onChange={this.handleChange}></textarea> 
                                  
                     </div>      
-                            <button class="postBtn">Post</button>
+                            <button className="postBtn">Post</button>
                             <button>Cancel</button>
                 </div> 
 
